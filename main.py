9c140b0c627b35c3c97c501b9b8f9a5bb59b0561
ponc3138 <fponce@csumb.edu>
2024-04-24 15:37:44 -0700

    IDK

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("hello world")
+from flask import Flask, render_template, flash, redirect
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from datetime import datetime
+
+
+@app.route('/', methods=('GET', 'POST'))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'csumb-otter'
+bootstrap = Bootstrap(app)
+
+class Playlist(FlaskForm):
+    song_title = StringField(
+        'Song Title', 
+        validators=[DataRequired()]
+    )
+
+# playlist = []
+
+# def store_song(my_song):
+#     playlist.append(dict(
+#         song = my_song,
+#         date = datetime.today()
+#     ))
+
+# @app.route('/', methods=('GET', 'POST'))
+# def index():
+#     form = Playlist()
+#     if form.validate_on_submit():
+#         store_song(form.song_title.data)
+#         return redirect('/view_playlist')
+#     return render_template('index.html', form=form)
