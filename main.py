38f75e83efb79ed0880f3d4bf194de9b0b75b942
ponc3138 <fponce@csumb.edu>
2024-05-15 14:01:48 -0700

    Added random movie button, and scroll bar

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-# # flask --app main --debug run
+"""https://rapidapi.com/rapihub-rapihub-default/api/imdb-top-100-movies/
+Used to populate top_100_movies variable
+
+"""
+
 from flask import Flask, render_template, request, session, redirect, url_for
 import pymysql
 import bcrypt
 import requests
-# import json 
+import random
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -73,25 +77,60 @@ def login():
 def logout():
     session.clear()
     return render_template('login.html', errorMessage='Error!!!')
-    
 
-@app.route('/searchInfo',methods=['GET', 'POST'])
+#Variable to store top 100 movies
+top_100_movies = []
+
+def get_top_100_movies():
+    url = "https://imdb-top-100-movies.p.rapidapi.com/"
+    headers = {
+        "X-RapidAPI-Key": "fd68b4fcb2mshd3c6bd86561d448p19fcb6jsna886e963e385",
+        "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
+
+def ensure_movies_loaded():
+    global top_100_movies
+    if not top_100_movies:
+        top_100_movies = get_top_100_movies()
+
+
+@app.route('/searchInfo', methods=['GET', 'POST'])
 def search_info():
+    # Ensure the movies are loaded
+    ensure_movies_loaded()
+
+    # Get 20 movies
+    random_movies = random.sample(top_100_movies, 20)
+
     if not is_authenticated():
         return redirect(url_for('login'))
-    return render_template('searchInfo.html', movieData=None)
+    return render_template('searchInfo.html', random_movies=random_movies)
 
 
 @app.route('/search', methods=['POST'])
 def search():
     if not is_authenticated():
         return redirect(url_for('login'))
-    movie_title = request.form['title']
-    print(movie_title)
+
+    ensure_movies_loaded()
+
+    if 'random' in request.form:
+        random_movie = random.choice(top_100_movies)
+        movie_title = random_movie['title']
+    else:
+        movie_title = request.form['title']
+
     api_key = 'f3b20a94'
     response = requests.get(f'http://www.omdbapi.com/?apikey={api_key}&t={movie_title}')
     movie_data = response.json()
     return render_template('search.html', movieData=movie_data)
+
+
 
 @app.route('/')
 def index():
@@ -114,15 +153,15 @@ def new_user_info():
         
         try:
             with connection.cursor() as cursor:
-                # SQL query to insert username and hashed password into the table
+                # SQL query to insert username and hashed password into the database
                 sql = "INSERT INTO p_admin (username, password) VALUES (%s, %s)"
-                # Execute the query with username and hashed password as parameters
+                # Execute query with username and hashed password 
                 cursor.execute(sql, (username, hashed_password))
                 connection.commit()
         except Exception as e:
             # If an error occurs
             connection.rollback()
-            # Handle the error
+            # Handle error
             print("Error:", e)
         finally:
             # Close database connection
